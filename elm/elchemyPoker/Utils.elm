93b912605fdaa196106346c1e5ea299d7245f544
Ilja Tkachuk <timcf@Iljas-iMac.local>
2017-12-21 17:53:25 +0200

    add Utils.getVariations function

--- elm/elchemyPoker/Utils.elm
+++ elm/elchemyPoker/Utils.elm
@@ -1,6 +1,43 @@
-module ElchemyPoker.Utils exposing (..)
+module ElchemyPoker.Utils exposing (shuffle, getVariations)
 import Elchemy exposing (ffi)
 
 shuffle : List a -> List a
 shuffle =
   ffi "Erlng" "shuffle"
+
+
+{-| Get list of variations.
+Limitations: k >= 0, (List.length set) >= k
+    getVariations 2 [ "a", "b" ] == Ok [["a","b"],["b","a"]]
+-}
+getVariations : Int -> List a -> Result String (List (List a))
+getVariations k set =
+    if k < 0 then
+        Err <| "getVariations limitation: k >= 0, your k = " ++ toString k
+    else if List.length set < k then
+        Err <|
+            "getVariations limitation: (List.length set) >= k, your set = "
+                ++ toString set
+                ++ " and your k = "
+                ++ toString k
+    else
+        Ok (getVariations_ k set)
+
+
+getVariations_ : Int -> List a -> List (List a)
+getVariations_ k set =
+    let
+        doGetVariations k set depth resultItem =
+            if depth < k then
+                set
+                    |> List.concatMap
+                        (\setItem ->
+                            if List.member setItem resultItem then
+                                []
+                            else
+                                doGetVariations k set (depth + 1) (setItem :: resultItem)
+                        )
+            else
+                [ resultItem |> List.reverse ]
+    in
+    doGetVariations k set 0 []
--- lib/elchemyPoker/Utils.elchemy.ex
+++ lib/elchemyPoker/Utils.elchemy.ex
@@ -9,5 +9,35 @@ defmodule ElchemyPoker.Utils do
   curry shuffle/1
   verify as: Erlng.shuffle/1
   def shuffle(a1), do: Erlng.shuffle(a1)
+  @moduledoc """
+  Get list of variations.
+  Limitations: k >= 0, (List.length set) >= k
+      iex> import ElchemyPoker.Utils
+      iex> get_variations(2, ["a", "b"])
+      {:ok, [["a", "b"], ["b", "a"]]}
+
+  
+  """
+  @spec get_variations(integer, list(any)) :: Elchemy.XResult.result(String.t, list(list(any)))
+  curry get_variations/2
+  def get_variations(k, set) do
+    cond do
+      (k < 0) -> (("getVariations limitation: k >= 0, your k = " ++ to_string().(k))
+        |> (fn x1 -> {:error, x1} end).())
+      (Elchemy.XList.length(set) < k) -> (("getVariations limitation: (List.length set) >= k, your set = " ++ (to_string().(set) ++ (" and your k = " ++ to_string().(k))))
+        |> (fn x1 -> {:error, x1} end).())
+      true -> {:ok, get_variations_(k, set)}
+    end
+  end
+
+  @spec get_variations_(integer, list(any)) :: list(list(any))
+  curry get_variations_/2
+  def get_variations_(k, set) do
+    do_get_variations = rec do_get_variations, fn k -> fn set -> fn depth -> fn result_item -> if (depth < k) do (set
+    |> (Elchemy.XList.concat_map().(fn set_item -> if Elchemy.XList.member(set_item, result_item) do [] else do_get_variations.(k).(set).((depth + 1)).([set_item | result_item]) end end)).()) else [(result_item
+    |> (Elchemy.XList.reverse).())] end end end end end
+    do_get_variations.(k).(set).(0).([])
+  end
+
 end
 
